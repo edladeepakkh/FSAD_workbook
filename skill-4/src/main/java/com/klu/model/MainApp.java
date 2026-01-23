8c9d54df9734148643666b0b84fd6cba49e66c5c
Deepakkh <deepakkhedla32@gmail.com>
2026-01-23 08:35:55 +0530

    Added FSAD skill projects

---
+++ skill-4/src/main/java/com/klu/model/MainApp.java
@@ -0,0 +1,16 @@
+package com.klu.model;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean.xml");
+
+        Student student = context.getBean("student", Student.class);
+        student.display();
+    }
+}
